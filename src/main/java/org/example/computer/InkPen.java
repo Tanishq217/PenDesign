3b79304f63e6072f82e4c1fae4edb064f04ec563
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2026-03-30 07:22:41 +0530

    Add pen factory for client object creation Implements Factory Pattern for pen instantiation Centralizes object creation logic Client only needs to specify type, color, and mechanism Encapsulates creation complexity"

--- src/main/java/org/example/computer/InkPen.java
+++ src/main/java/org/example/computer/InkPen.java
@@ -1,15 +1,11 @@
 package org.example.computer;
 
-import com.pen.system.*;
-import com.pen.system.strategy.InkWritingStrategy;
-import com.pen.system.refill.InkRefill;
-
 public class InkPen extends AbstractPen {
-    public InkPen(StartingStrategy startingStrategy, InkColor color) {
+    public InkPen(StartingStrategy startingStrategy, Enums.InkColor color) {
         super(new InkWritingStrategy(),
                 startingStrategy,
                 new InkRefill(color.toString()),
                 color,
-                PenType.INK);
+                Enums.PenType.INK);
     }
 }
---
+++ src/main/java/org/example/computer/PenFactory.java
@@ -0,0 +1,33 @@
+package org.example.computer;
+
+
+public class PenFactory {
+
+    public static Pen createPen(Enums.PenType type, Enums.InkColor color, Enums.StartingMechanism mechanism) {
+        StartingStrategy startStrategy;
+
+        // Create appropriate starting mechanism
+        switch (mechanism) {
+            case CAP:
+                startStrategy = new CapMechanism();
+                break;
+            case CLICK:
+                startStrategy = new ClickMechanism();
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown mechanism: " + mechanism);
+        }
+
+        // Create appropriate pen type
+        switch (type) {
+            case BALLPOINT:
+                return new BallpointPen(startStrategy, color);
+            case INK:
+                return new InkPen(startStrategy, color);
+            case GEL:
+                return new GelPen(startStrategy, color);
+            default:
+                throw new IllegalArgumentException("Unknown pen type: " + type);
+        }
+    }
+}
